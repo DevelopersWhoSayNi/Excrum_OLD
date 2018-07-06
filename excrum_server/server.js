cb67fd114fbd9ad88210cae777100ab544cf7bfa
Mariane Pires Pinheiro <mariane.pires@exact.com>
2018-07-06 16:56:40 +0200

    first commit

---
+++ excrum_server/server.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port);
+console.log(`We run at port: ${port}`);
