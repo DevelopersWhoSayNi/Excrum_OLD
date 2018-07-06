334ededebafd4203fba2271f18539b745ddbefc2
Mariane Pires Pinheiro <mariane.pires@exact.com>
2018-07-06 14:45:58 +0200

    test

--- excrum/src/user/UserActions.js
+++ excrum/src/user/UserActions.js
@@ -3,3 +3,7 @@ import * as types from '../ActionTypes';
 export function addUser(userName, password) {
   return { type: types.AddUser, userName, password };
 }
+
+export function addUser2(userName, password) {
+  return { type: types.AddUser, userName, password };
+}
