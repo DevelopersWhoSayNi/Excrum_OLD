64b51c17f7500e819cb6e6467321b26ad3a32a21
Mariane Pires Pinheiro <mariane.pires@exact.com>
2018-07-06 15:12:22 +0200

    undo test

--- excrum/src/user/UserActions.js
+++ excrum/src/user/UserActions.js
@@ -3,7 +3,3 @@ import * as types from '../ActionTypes';
 export function addUser(userName, password) {
   return { type: types.AddUser, userName, password };
 }
-
-export function addUser2(userName, password) {
-  return { type: types.AddUser, userName, password };
-}
