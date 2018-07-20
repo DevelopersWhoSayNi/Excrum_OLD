30ed4376b32f55227e37b81352c07e4a87061568
Mariane Pires Pinheiro <mahrinoa@gmail.com>
2018-07-20 13:15:09 +0200

    try to see the user email change

--- excrum_server/server.js
+++ excrum_server/server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT || 8000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("NI!"));
 
 app.listen(port);
 console.log(`We run at port: ${port}`);
