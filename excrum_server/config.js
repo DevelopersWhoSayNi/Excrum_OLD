c230b8ecab73a5ca37ac57a452f3abf087f47d2d
ecs\KHAL360266 <amir.khalajestani@exact.com>
2018-08-14 09:38:39 +0200

    config change

--- excrum_server/config.js
+++ excrum_server/config.js
@@ -1,4 +1,4 @@
 var credentials = process.env.mlab_key;
 module.exports = {
-  url: "mongodb://" + credentials + "@ds231941.mlab.com:31941/excrum"
+  url: `mongodb://${credentials}@ds231941.mlab.com:31941/excrum`
 };
